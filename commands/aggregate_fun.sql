fd81b9de43027590cc8b2313e0e409ff37a201b5
iproman <itechman@yandex.ru>
2018-08-07 22:59:59 +0300

    Count

--- commands/aggregate_fun.sql
+++
@@ -1,7 +0,0 @@
--- SELECT COUNT(*) FROM books;
-
--- SELECT COUNT(DISTINCT author_fname) FROM books;
-
--- SELECT COUNT(DISTINCT author_lname, author_fname) FROM books;
-
--- SELECT title FROM BOOK
---
+++ commands/count.sql
@@ -0,0 +1,12 @@
+-- 10
+
+SELECT COUNT(*) FROM books;
+
+SELECT COUNT(DISTINCT author_fname) FROM books;
+
+SELECT COUNT(DISTINCT author_lname, author_fname) FROM books;
+
+SELECT
+  COUNT(title)
+  FROM books
+  WHERE title LIKE '%the%';
