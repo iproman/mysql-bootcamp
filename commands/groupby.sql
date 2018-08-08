7510eddbba8ee3da49c5a1c1e54a836fc62c0292
iproman <itechman@yandex.ru>
2018-08-08 22:38:39 +0300

    Group by command

--- commands/groupby.sql
+++ commands/groupby.sql
@@ -6,3 +6,23 @@ SELECT title, author_fname, author_lname FROM books GROUP BY author_lname;
 
 SELECT author_fname, author_lname, COUNT(*)
 FROM books GROUP BY author_lname, author_fname;
+
+
++----------------------------+
+| books                      |
++----------------------------+
+| In 1945 1 book(s) released |
+| In 1981 1 book(s) released |
+| In 1985 1 book(s) released |
+...
+
+SELECT
+  CONCAT(
+    'In ',
+    released_year,
+    ' ',
+    COUNT(*),
+    ' book(s) released'
+  ) AS books
+FROM books
+GROUP BY released_year;
