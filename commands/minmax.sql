39652b55e08f4478b40900f29df2cc5ee8c9f6c0
iproman <itechman@yandex.ru>
2018-08-09 23:45:38 +0300

    Minmax command

--- commands/minmax.sql
+++ commands/minmax.sql
@@ -10,3 +10,15 @@ SELECT title, pages FROM books WHERE pages =
 
 -- faster
 SELECT title,pages FROM books ORDER BY pages ASC LIMIT 1;
+
+SELECT author_fname, author_lname,
+  MIN(released_year)
+FROM books
+GROUP BY author_lname, author_fname;
+
+-- slower
+SELECT title, pages FROM books WHERE pages =
+(SELECT MIN(pages) FROM books);
+
+-- faster
+SELECT title,pages FROM books ORDER BY pages ASC LIMIT 1;
