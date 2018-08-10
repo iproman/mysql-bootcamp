a369ba186b0cf63e966056e22828255e156d6101
iproman <itechman@yandex.ru>
2018-08-11 00:44:48 +0300

    AVG commands

--- commands/avg.sql
+++ commands/avg.sql
@@ -2,6 +2,10 @@
 
 SELECT AVG(pages) FROM BOOKS;
 
-SELECT AVG(stock_quantity)
+SELECT released_year, AVG(stock_quantity)
 FROM books
 GROUP BY released_year;
+
+SELECT author_fname, author_lname ,AVG(pages)
+FROM books
+GROUP BY author_lname, author_fname;
