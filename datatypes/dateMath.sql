95fbe286116e8f85085edb2339261d803ec550ba
iproman <itechman@yandex.ru>
2018-08-16 23:15:13 +0300

    Date Math

--- datatypes/dateMath.sql
+++ datatypes/dateMath.sql
@@ -1,1 +1,5 @@
 SELECT DATEDIFF(birthdt, NOW()) from people;
+
+SELECT birthdt, DATE_ADD(birthdt, INTERVAL -1 MONTH ) FROM people;
+
+SELECT birthdt, DATE_ADD(birthdt, INTERVAL 1 MONTH + INTERVAL 1 YEAR) FROM people;
