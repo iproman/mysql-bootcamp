146ff8bdd7290c5057796dfbbe4d66d15e8e0f39
iproman <itechman@yandex.ru>
2018-08-18 17:40:05 +0300

    Example 5

---
+++ ex/ex5.sql
@@ -0,0 +1,60 @@
+-- ex5
+
+-- Ex 1
+-- What's a good use case for
+-- CHAR?
+-- Just make one up
+
+-- Ex 2
+-- Fill in the blanks
+CREATE TABLE inventory (
+  item_name _________,
+  price _____________,
+  quantity __________,
+);
+-- price is always < 1,000,000
+
+-- Ex 3
+-- What is the
+-- difference between
+-- DATETIME and TIMESTAMP?
+
+-- Ex 4
+-- Print out the
+-- current time
+
+-- Ex 5
+-- Print out the current
+-- date(but not time)
+
+-- Ex 6
+-- Print out the current
+-- day of the week
+-- (the number)
+
+-- Ex 7
+-- Print out the current
+-- day of the week
+-- (the day name)
+
+-- Ex 8
+-- Print out he current
+-- day and time using
+-- this format:
+-- mm/dd/yyyy
+
+
+-- Ex 9
+-- Print out the current
+-- day and time using
+-- this format:
+-- January 2nd at 3:15
+-- April 1st at 10:18
+
+
+
+
+
+
+
+
