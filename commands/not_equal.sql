14bf7f494edddbd05440273167a3c3151194cf6a
iproman <itechman@yandex.ru>
2018-08-19 23:59:46 +0300

    Not equal

--- commands/not_equal.sql
+++ commands/not_equal.sql
@@ -1,1 +1,11 @@
-SELECT title, released_year FROM books WHERE released_year != 2017;
+SELECT
+  title,
+  released_year
+FROM books
+WHERE released_year != 2017;
+
+
+SELECT title, author_lname
+FROM books AS excepting
+WHERE author_lname != 'harris'
+;
