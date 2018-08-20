6d51372b0f166efff586db1334197e12f385eabb
iproman <itechman@yandex.ru>
2018-08-20 22:32:19 +0300

    Greater than >

--- commands/greater_than.sql
+++ commands/greater_than.sql
@@ -1,10 +1,19 @@
 SELECT title, released_year
 FROM books as rel
-ORDER BY released_year;
-
+ORDER BY released_year
+;
 
 SELECT title, released_year
 FROM books
 WHERE released_year > 2000
 ORDER BY released_year
 ;
+
+SELECT title, stock_quantity
+FROM books
+WHERE stock_quantity >= 100
+;
+
+SELECT title, author_lname
+FROM books
+WHERE author_lname = 'Eggers';
