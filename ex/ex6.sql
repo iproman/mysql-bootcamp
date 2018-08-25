b42007a81a3cd06ca41fdf6b67f4d21ee1614c97
iproman <itechman@yandex.ru>
2018-08-26 01:07:47 +0300

    Ex 6

--- ex/ex6.sql
+++ ex/ex6.sql
@@ -5,3 +5,42 @@
 SELECT 10 != 10; -- 0
 SELECT 15 > 14 && 99-5 <= 94; -- 1
 SELECT 1 IN (5,3) || 9 BETWEEN 8 AND 10; -- 0 (0, 1)
+
+-- Ex 2
+-- Select all books written
+-- before 1980 (non inclusive)
+
+-- Ex 3
+-- Select all books
+-- written by Eggers or Chabon
+
+-- Ex 4
+-- Select all books
+-- written by Lahiri,
+-- Published after 2000
+
+-- Ex 5
+-- Select all books with
+-- page counts between
+-- 100 and 200
+
+-- Ex 6
+-- Select all books where
+-- author_lname starts
+-- with a 'C' or an 'S'
+
+-- Ex 7
+-- If title contains 'stories' -> Short Stories
+-- Just kids and a Hearthbreaking Work -> Memoir
+-- Everything Else -> Novel
+-- +-----------------------------------------------------+----------------+---------------+
+-- | title                                               | author_lname   | TYPE          |
+-- +-----------------------------------------------------+----------------+---------------+
+-- | The Namesake                                        | Lahiri         | Novel         |
+-- | Norse Mythology                                     | Gaiman         | Novel         |
+-- | Just Kids                                           | Smith          | Memoir        |
+-- | A Heartbreaking Work of Staggering Genius           | Eggers         | Memoir        |
+-- | Coraline                                            | Gaiman         | Novel         |
+-- | What We Talk About When We Talk About Love: Stories | Carver         | Short Stories |
+-- | Where I'm Calling From: Selected Stories            | Carver         | Short Stories |
+-- ...
--- ex/ex6s.sql
+++ ex/ex6s.sql
@@ -1,1 +1,44 @@
 -- Solution 1
+-- 0, 1, 0 (0, 1)
+
+-- Solution 2
+SELECT title, released_year
+FROM books
+WHERE released_year < 1980
+;
+
+-- Solution 3
+SELECT title, author_lname
+FROM books
+WHERE author_lname = 'Eggers' OR
+author_lname = 'Chabon'
+;
+
+-- Solution 4
+SELECT title, author_lname, released_year
+FROM books
+WHERE author_lname = 'Lahiri' AND
+released_year > 2000
+;
+
+-- Solution 5
+SELECT title, pages
+FROM books
+WHERE pages BETWEEN 100 AND 200;
+
+-- Solution 6
+SELECT author_lname
+FROM books
+WHERE author_lname LIKE 'C%'
+OR author_lname LIKE 'S%';
+
+-- Solution 7
+SELECT title, author_lname,
+  CASE
+    WHEN title LIKE '%stories%' THEN 'Short Stories'
+    WHEN title LIKE '%just kids%' THEN 'Memoir'
+    WHEN title LIKE '%a heartbreaking work%' THEN 'Memoir'
+    ELSE 'Novel'
+  END AS 'TYPE'
+FROM books
+;
