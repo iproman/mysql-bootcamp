ee5f2da49792a48556f983908affb669e7cb8362
iproman <itechman@yandex.ru>
2018-08-29 00:32:12 +0300

    Inner join command

--- relationships/inner_join.sql
+++ relationships/inner_join.sql
@@ -1,3 +1,26 @@
-SELECT * FROM customers, orders
+-- IMPLICIT INNER JOIN
+SELECT * FROM orders, customers
 WHERE
 orders.customer_id=customers.id;
+
+SELECT
+  order_date,
+  CONCAT(first_name, ' ', last_name) AS 'Full name',
+  amount
+FROM orders, customers
+WHERE orders.customer_id=customers.id;
+
+
+-- EXPLICIT INNER JOIN
+SELECT
+  order_date,
+  CONCAT(first_name, ' ', last_name) AS 'Full name',
+  amount
+FROM customers
+JOIN orders
+  ON orders.customer_id = customers.id;
+
+-- ARBITRARY JOIN - don't do that
+SELECT * FROM customers
+JOIN orders
+  ON orders.customer_id = customers.id;
