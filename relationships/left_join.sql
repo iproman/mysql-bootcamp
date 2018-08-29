10eadf8e7f41391cbf03146c3414f01870f42df9
iproman <itechman@yandex.ru>
2018-08-29 23:22:45 +0300

    Left join

--- relationships/left_join.sql
+++ relationships/left_join.sql
@@ -8,11 +8,34 @@ JOIN orders
   ON orders.customer_id = customers.id
   ORDER BY order_date DESC;
 
-  SELECT
+SELECT
   order_date,
   CONCAT(first_name, ' ', last_name) AS 'Full name',
-  amount
+  SUM(amount) AS total_spent
 FROM customers
 JOIN orders
-  ON orders.customer_id = customers.id
-  GROUP BY orders.customer_id;
+  ON customers.id = orders.customer_id
+GROUP BY orders.customer_id, order_date
+ORDER BY total_spent DESC;
+
+SELECT * FROM customers
+INNER JOIN orders
+  ON customers.id = orders.customer_id;
+
+SELECT * FROM customers
+LEFT JOIN orders
+  ON customers.id = orders.customer_id;
+
+SELECT first_name, last_name, order_date, amount as a FROM customers
+LEFT JOIN orders
+  ON customers.id = orders.customer_id;
+
+SELECT
+  first_name,
+  last_name,
+  IFNULL(SUM(amount), 0) AS total_spent
+FROM customers
+LEFT JOIN orders
+  ON customers.id = orders.customer_id
+GROUP BY customers.id, order_date
+ORDER BY total_spent;
