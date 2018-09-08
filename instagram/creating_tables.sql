0dbde5a7da32555d3e20dae50751723c6b3f272d
iproman <itechman@yandex.ru>
2018-09-08 23:25:52 +0300

    Create tables for instag_clone

--- instagram/creating_tables.sql
+++ instagram/creating_tables.sql
@@ -1,8 +1,29 @@
 -- CREATE DATABASE instag_clone;
 -- USE instag_clone;
 
-CREATE TABLE users {
+CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) UNIQUE NOT NULL,
   created_at TIMESTAMP DEFAULT NOW()
-};
+);
+
+CREATE TABLE photos(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  image_url VARCHAR(255) NOT NULL ,
+  user_id INT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY(user_id)
+    REFERENCES users(id)
+);
+
+CREATE TABLE comments(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  comment_text VARCHAR(255) NOT NULL,
+  user_id INT NOT NULL,
+  photo_id INT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY(user_id)
+    REFERENCES users(id),
+  FOREIGN KEY(photo_id)
+    REFERENCES photos(id)
+);
