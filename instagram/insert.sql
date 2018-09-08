f5743ffd2bf326c76d718b8d8d3bca05ca5947ff
iproman <itechman@yandex.ru>
2018-09-08 23:26:08 +0300

    Insert examples

---
+++ instagram/insert.sql
@@ -0,0 +1,26 @@
+-- INSERT INTO users(
+--   username
+-- ) VALUES
+--   ('BlutTheCat'),
+--   ('CharlieBrown'),
+--   ('ColtSteele');
+
+-- INSERT INTO photos(
+--   image_url, user_id
+-- ) VALUES
+--   ('/sdfsdfs', 1),
+--   ('/sdf1fs', 1),
+--   ('/90qwer', 2);
+
+-- INSERT INTO comments(
+--   comment_text,
+--   user_id,
+--   photo_id
+-- ) VALUES
+--   ('Meow!', 1, 2),
+--   ('Amazing SHot!', 3, 2),
+--   ('I <3 This !', 2, 1);
+
+CREATE TABLE likes(
+
+)
