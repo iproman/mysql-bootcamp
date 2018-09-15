5251b743c96bec0398641b5af436682f6fad4a48
iproman <itechman@yandex.ru>
2018-09-16 00:13:39 +0300

    Exercises 8

---
+++ ex/ex8.sql
@@ -0,0 +1,42 @@
+-- Generate 500 emails.
+
+-- 1. Find earliest date a user joined
+-- +--------------------------------+---------------------+
+-- | email                          | created_at          |
+-- +--------------------------------+---------------------+
+-- | Scottie_McCullough24@yahoo.com | 2017-09-20 07:45:07 |
+-- +--------------------------------+---------------------+
+
+-- 2. Find email of the first earliest User
+-- +--------------------------------+
+-- | email                          |
+-- +--------------------------------+
+-- | Scottie_McCullough24@yahoo.com |
+-- +--------------------------------+
+
+-- 3. Users according to the month they joined
+-- +-----------+-------+
+-- | month     | count |
+-- +-----------+-------+
+-- | April     |    38 |
+-- | August    |    45 |
+-- | December  |    36 |
+-- | February  |    45 |
+-- | January   |    49 |
+-- | July      |    47 |
+-- | June      |    42 |
+-- | March     |    42 |
+-- | May       |    33 |
+-- | November  |    40 |
+-- | October   |    46 |
+-- | September |    37 |
+-- +-----------+-------+
+
+-- 4. Count number of users with yahoo emails
+-- +-------------+
+-- | yahoo_users |
+-- +-------------+
+-- |         170 |
+-- +-------------+
+
+-- 5. Calculate Total Number of users for each email host
