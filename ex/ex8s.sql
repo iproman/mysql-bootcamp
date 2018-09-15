68e58b2ef4442ef28e2feb3475bcfd95bff5385a
iproman <itechman@yandex.ru>
2018-09-16 00:13:56 +0300

    Exercises 8 solutions

---
+++ ex/ex8s.sql
@@ -0,0 +1,37 @@
+-- 1.
+SELECT * FROM users ORDER BY created_at LIMIT 1;
+
+-- 2.
+SELECT email FROM users ORDER BY created_at LIMIT 1;
+
+-- 3.
+SELECT
+  DATE_FORMAT(created_at, '%M') as month,
+  COUNT(*) as count
+FROM users
+GROUP BY month
+ORDER BY month;
+
+-- 4.
+SELECT
+  COUNT(*) as 'yahoo_users'
+FROM users
+WHERE email LIKE '%yahoo.com'
+;
+
+-- 5
+SELECT
+  CASE
+    WHEN email LIKE '%gmail'
+      THEN 'gmail'
+    WHEN email LIKE '%yahoo'
+      THEN 'yahoo'
+    WHEN email LIKE '%hotmail'
+      THEN 'hotmail'
+    ELSE 'other'
+  END AS 'provider'
+--   COUNT('provider') as 'total_users'
+FROM users
+-- GROUP BY 'provider',
+;
+
